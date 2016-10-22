d71a10e573b1357d0c2de21280db5f776250bd5b
Ryan Bates <ryan.bates@students.aie.edu.au>
2016-10-21 20:08:23 -0500

    created movement script

---
+++ Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterMovement : MonoBehaviour
+{
+
+    Vector3 Jump = new Vector3(0, 100, 0);
+    
+	
+    void Update ()
+    {
+        if(Input.GetKey(KeyCode.W))
+            gameObject.transform.position += new Vector3(0, 0, 5) * Time.deltaTime;
+        if (Input.GetKey(KeyCode.A))
+            gameObject.transform.position += new Vector3(-5, 0, 0) * Time.deltaTime;
+        if (Input.GetKey(KeyCode.S))
+            gameObject.transform.position += new Vector3(0, 0, -5) * Time.deltaTime;
+        if (Input.GetKey(KeyCode.D))
+            gameObject.transform.position += new Vector3(5, 0, 0) * Time.deltaTime;
+        if (Input.GetKeyDown(KeyCode.Space))
+            gameObject.transform.position += (Jump * Time.deltaTime); 
+    }
+}
