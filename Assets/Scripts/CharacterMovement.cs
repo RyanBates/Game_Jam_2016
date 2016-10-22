16b84eef5cd23ee44247a914b1245a7ad2881f53
Ryan Bates <ryan.bates@students.aie.edu.au>
2016-10-22 14:18:17 -0500

    changes to movement script

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -3,11 +3,19 @@ using System.Collections;
 
 public class CharacterMovement : MonoBehaviour
 {
+    public CharacterController character;
     float currentTime = 0;
     float previousTime = 0;
     float deltaTime = 0;
 
-    Vector3 Jump = new Vector3(0, 80, 0);
+    float Jump;
+    float jumpforce = 10;
+    float gravity = 14;
+
+    private void Start()
+    {
+        character = GetComponent<CharacterController>();
+    }
 
     void Update()
     {
@@ -25,9 +33,16 @@ public class CharacterMovement : MonoBehaviour
             gameObject.transform.position += new Vector3(0, 0, -5) * deltaTime;
         if (Input.GetKey(KeyCode.D))
             gameObject.transform.position += new Vector3(5, 0, 0) * deltaTime;
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (character.isGrounded)
         {
-                gameObject.transform.position += Jump * deltaTime; 
+            Jump = -gravity * Time.deltaTime;
+            if (Input.GetKeyDown(KeyCode.Space))
+                Jump = jumpforce;
+
+            else
+                Jump -= gravity * Time.deltaTime;
         }
+        Vector3 name = new Vector3(0, Jump, 0);
+        character.Move(name * Time.deltaTime);
     }
 }
