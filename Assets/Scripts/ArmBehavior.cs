169047ba471fbb08f95bc352b9df5d15bf47d9d8
Rory Vaughn <Rvaughn2525@gmail.com>
2016-10-21 22:34:28 -0500

    basic arm mechanic

--- Assets/Scripts/ArmBehavior.cs
+++ Assets/Scripts/ArmBehavior.cs
@@ -6,6 +6,15 @@ public class ArmBehavior : MonoBehaviour {
     private float Radius = 3;
     public Vector3 Displacement;
     public Vector3 Displacement2;
+    float speed = 20.0f;
+    float zPositionLock;
+    float xPositionLock;
+    bool wkey = false;
+    bool skey = false;
+    bool qkey = false;
+    bool ekey = false;
+    bool dkey = false;
+    bool akey = false;
     public GameObject PlayerBody;
 	// Use this for initialization
 	void Start () {
@@ -16,19 +25,79 @@ public class ArmBehavior : MonoBehaviour {
     void Update()
     {
         Displacement = (gameObject.transform.position - PlayerBody.transform.position);
-        
-        if (Displacement.x >= Radius )
+        wkey = false;
+        skey = false;
+        qkey = false;
+        ekey = false;
+        dkey = false;
+        akey = false;
+
+        Vector3 direction = Vector3.zero;
+
+        if (Input.GetKey(KeyCode.W) && transform.position.z < Radius + PlayerBody.transform.position.z)
         {
-            gameObject.transform.position = new Vector3(gameObject.transform.position.x - Displacement.x, gameObject.transform.position.y, gameObject.transform.position.z);
+            wkey = true;
         }
-        if (Displacement.y >= Radius)
+
+        if (Input.GetKey(KeyCode.S) && transform.position.z > -Radius + PlayerBody.transform.position.z)
         {
-            gameObject.transform.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y - Displacement.y, gameObject.transform.position.z);
+            skey = true;
         }
-        if (Displacement.z >= Radius)
+        if (Input.GetKey(KeyCode.Q) && transform.position.y < Radius + PlayerBody.transform.position.y)
         {
-            gameObject.transform.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, gameObject.transform.position.z - Displacement.z);
+            qkey = true;
         }
+        if (Input.GetKey(KeyCode.E) && transform.position.y > -Radius + PlayerBody.transform.position.y)
+        {
+            ekey = true;
+        }
+
+        if (Input.GetKey(KeyCode.D) && transform.position.x < Radius + PlayerBody.transform.position.x)
+        {
+            dkey = true;
+        }
+
+        if (Input.GetKey(KeyCode.A) && transform.position.x > -Radius + PlayerBody.transform.position.x)
+        {
+            akey = true;
+        }
+        if (wkey == true)
+        {
+            direction += new Vector3(0, 0, 1);
+        }
+        if (skey == true)
+        {
+            direction += new Vector3(0, 0, -1);
+        }
+        if (qkey == true)
+        {
+            direction += new Vector3(0, 1, 0);
+        }
+        if (ekey == true)
+        {
+            direction += new Vector3(0, -1, 0);
+        }
+        if (dkey == true)
+        {
+            direction += new Vector3(1, 0, 0);
+        }
+        if (akey == true)
+        {
+            direction += new Vector3(-1, 0, 0);
+        }
+        transform.position += direction * speed * Time.deltaTime;
+        //if (Displacement.x >= Radius )
+        //{
+        //    gameObject.transform.position = new Vector3(PlayerBody.transform.position.x + Displacement.x, PlayerBody.transform.position.y, PlayerBody.transform.position.z);
+        //}
+        //if (Displacement.y >= Radius)
+        //{
+        //    gameObject.transform.position = new Vector3(PlayerBody.transform.position.x, PlayerBody.transform.position.y + Displacement.y, PlayerBody.transform.position.z);
+        //}
+        //if (Displacement.z >= Radius)
+        //{
+        //    gameObject.transform.position = new Vector3(PlayerBody.transform.position.x, PlayerBody.transform.position.y, PlayerBody.transform.position.z + Displacement.z);
+        //}
 
     }
 }
