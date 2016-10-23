0509bfea118d46b36775674e86316b8b91a08a5f
Logan Pletcher <lpletch95@hotmail.com>
2016-10-23 00:38:48 -0500

    Update

--- Assets/MatchScript.cs
+++ Assets/MatchScript.cs
@@ -1,11 +1,28 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System.Collections.Generic;
 
 public class MatchScript : MonoBehaviour {
-
+    List<GameObject> Tags;
+    GameObject Clock;
 	// Use this for initialization
 	void Start () {
-	
+        foreach(GameObject gO in FindObjectsOfType<GameObject>())
+        {
+            string ShortenedName = "";
+            for(int i = 0; i < 5; i++)
+            {
+                ShortenedName += gO.name[i];
+            }
+            if(ShortenedName == "Player")
+            {
+                Tags.Add(gO);
+            }
+            if(ShortenedName == "Clock")
+            {
+                Clock = gO;
+            }
+        }
+
 	}
 	
 	// Update is called once per frame
